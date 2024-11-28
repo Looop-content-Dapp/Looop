705df93b7a11d69e4eac194c35cb016922d5b5ac
Senninseyi <seyi.oyebamiji@gmail.com>
2024-11-28 18:00:41 +0100

    CHORE: update signin screen

--- app/(auth)/signin.tsx
+++ app/(auth)/signin.tsx
@@ -1,25 +1,32 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React, { useContext, useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { TextInput } from 'react-native-gesture-handler'
-import { router } from 'expo-router'
-import { useClerkAuthentication } from '../../hooks/useClerkAuthentication';
-import {
-    ViewIcon,
-    ViewOffIcon,
-  } from "@hugeicons/react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TextInput } from "react-native-gesture-handler";
+import { useClerkAuthentication } from "../../hooks/useClerkAuthentication";
+import { ViewIcon, ViewOffIcon } from "@hugeicons/react-native";
+import { AppButton } from "@/components/app-components/button";
 
 const signin = () => {
-    const [emailAddress, setEmailAddress] = React.useState('');
-    const [password, setPassword] = useState("")
-    const [passwordView, setPasswordView] = useState(false);
-    const {handleAppleSignUp, handleEmailSignUp, handleGoogleSignUp, handleEmailSignIn, loading} = useClerkAuthentication()
+  const [emailAddress, setEmailAddress] = React.useState("");
+  const [password, setPassword] = useState("");
+  const [passwordView, setPasswordView] = useState(false);
+
+  const {
+    handleAppleSignUp,
+    handleEmailSignUp,
+    handleGoogleSignUp,
+    handleEmailSignIn,
+    loading,
+  } = useClerkAuthentication();
   return (
-    <SafeAreaView style={{
+    <SafeAreaView
+      style={{
         flex: 1,
         paddingHorizontal: 24,
-        maxHeight: "100%"
-    }} className="flex-1 min-h-full px-6">
+        maxHeight: "100%",
+      }}
+      className="flex-1 min-h-full px-6"
+    >
       <View className="items-center">
         <Image
           source={require("../../assets/images/logo-orange.png")}
@@ -30,16 +37,16 @@ const signin = () => {
       <View className="gap-y-12 mt-6">
         <View className="gap-y-2 pt-6">
           <Text className="text-white text-2xl font-PlusJakartaSansBold">
-           Welcome back!
+            Welcome back!
           </Text>
           <Text className="text-[#D2D3D5] text-[16px]font-PlusJakartaSansRegular">
-           Dive right in and continue exploring
+            Dive right in and continue exploring
           </Text>
         </View>
 
         <View className="gap-y-3">
           <Text className="text-lg text-gray-200 font-PlusJakartaSansBold">
-           Email
+            Email
           </Text>
           <TextInput
             placeholderTextColor="#787A80"
@@ -47,35 +54,39 @@ const signin = () => {
             value={emailAddress}
             onChangeText={setEmailAddress}
             className="h-16 text-sm font-PlusJakartaSansRegular bg-Grey/07 text-Grey/04 rounded-full px-8"
+            inputMode="email"
+            keyboardAppearance="dark"
+            keyboardType="email-address"
           />
 
           <Text className="text-lg text-gray-200 font-PlusJakartaSansBold">
-           Password
+            Password
           </Text>
           <View className="flex-row items-center bg-Grey/07 overflow-hidden rounded-full pr-5">
-          <TextInput
-           secureTextEntry={passwordView}
-            placeholderTextColor="#787A80"
-            placeholder="Enter password"
-            value={password}
-            onChangeText={setPassword}
-            className="h-16 text-sm font-PlusJakartaSansRegular bg-Grey/07 text-Grey/04 flex-1 px-8"
-          />
-          <TouchableOpacity onPress={() => setPasswordView(!passwordView)}>
+            <TextInput
+              secureTextEntry={passwordView}
+              placeholderTextColor="#787A80"
+              placeholder="Enter password"
+              value={password}
+              onChangeText={setPassword}
+              className="h-16 text-sm font-PlusJakartaSansRegular bg-Grey/07 text-Grey/04 flex-1 px-8"
+            />
+            <TouchableOpacity onPress={() => setPasswordView(!passwordView)}>
               {passwordView ? (
                 <ViewIcon size={24} color="#787A80" variant="solid" />
               ) : (
                 <ViewOffIcon size={24} color="#787A80" variant="solid" />
               )}
-            </TouchableOpacity></View>
-
+            </TouchableOpacity>
+          </View>
         </View>
 
-        <TouchableOpacity onPress={() => handleEmailSignIn(emailAddress, password)}  className="bg-orange-500 items-center py-4 rounded-full">
-          <Text className="text-lg font-PlusJakartaSansMedium text-white">
-            {loading ? "Logging...." : "Continue"}
-          </Text>
-        </TouchableOpacity>
+        <AppButton.Primary
+          text="Continue"
+          loading={loading}
+          disabled={loading}
+          onPress={() => handleEmailSignIn(emailAddress, password)}
+        />
 
         <Text className="mt-[10px] text-center text-gray-400 font-PlusJakartaSansRegular text-sm">
           Or you can sign in with
@@ -83,7 +94,7 @@ const signin = () => {
       </View>
 
       <View className="mt-12 gap-y-4">
-        <TouchableOpacity  className="bg-white py-4 rounded-full flex-row items-center justify-center">
+        <TouchableOpacity className="bg-white py-4 rounded-full flex-row items-center justify-center">
           <Image
             source={require("../../assets/images/google.png")}
             className="w-8 h-8"
@@ -93,7 +104,7 @@ const signin = () => {
           </Text>
         </TouchableOpacity>
 
-        <TouchableOpacity  className="bg-white py-4 rounded-full flex-row items-center justify-center">
+        <TouchableOpacity className="bg-white py-4 rounded-full flex-row items-center justify-center">
           <Image
             source={require("../../assets/images/apple.png")}
             className="w-8 h-8"
@@ -104,13 +115,17 @@ const signin = () => {
         </TouchableOpacity>
       </View>
 
-      {/* <View className="flex-row items-center justify-center gap-x-[2px] absolute bottom-12 left-0 right-0">
-        <Text className="text-[#787A80] font-PlusJakartaSansMedium text-[16px]">Powered by</Text>
+      <View className="flex-row items-center justify-center gap-x-[2px] absolute bottom-12 left-0 right-0">
+        <Text className="text-[#787A80] font-PlusJakartaSansMedium text-[16px]">
+          Powered by
+        </Text>
         <Image source={require("../../assets/images/starknet.png")} />
-        <Text className="text-[#787A80] font-PlusJakartaSansMedium text-[16px]">Starknet</Text>
-      </View> */}
+        <Text className="text-[#787A80] font-PlusJakartaSansMedium text-[16px]">
+          Starknet
+        </Text>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default signin
+export default signin;
