ebeb00ccba58bf670261f4198276b4daed5c60fe
Ademola101 <ademolaogunmokun@yahoo.com>
2025-02-21 10:23:28 +0100

    capitalize text in onboarding content for consistency

--- app/onboarding2/index.tsx
+++ app/onboarding2/index.tsx
@@ -24,7 +24,7 @@ import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
 } from "react-native-responsive-screen";
-import CustomBottomComponent from "./BottomComponent";
+import { ONBOARDING_TEXTS } from "@/data/data";
 
 const pagesBackgroundColor = [
   "#FF6D1B",
@@ -63,6 +63,52 @@ const imageStyle: ImageStyle = {
   marginTop: -hp("30%"),
 };
 
+const CustomBottomContent = ({ pageIndex }: { pageIndex: number }) => {
+  return (
+    <View style={styles.subtitleWrapper}>
+      <View style={styles.bottomContainer}>
+        <View style={styles.textContainer}>
+          <Text className="text-white" style={styles.bigText}>
+            {ONBOARDING_TEXTS[pageIndex].first.text}
+            <Text
+              className="font-TankerRegular"
+              style={{ color: ONBOARDING_TEXTS[pageIndex].second.color }}
+            >
+              {ONBOARDING_TEXTS[pageIndex].second.text}
+            </Text>
+            {pageIndex === 3 && (
+              <Text
+                style={[
+                  styles.bigText,
+                  { color: ONBOARDING_TEXTS[pageIndex]?.third?.color },
+                ]}
+              >
+                {ONBOARDING_TEXTS[pageIndex]?.third?.text}
+              </Text>
+            )}
+          </Text>
+        </View>
+
+        <View style={styles.paginationContainer}>
+          {[0, 1, 2, 3].map((index) => (
+            <View
+              key={index}
+              style={[
+                styles.paginationDot,
+                pageIndex === index && styles.paginationDotActive,
+              ]}
+            />
+          ))}
+        </View>
+
+        <TouchableOpacity style={styles.button}>
+          <Text className="text-white">Continue to Sign in</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
 const Onboard2 = () => {
   const [pageIndex, setPageIndex] = useState<number>(0);
 
@@ -93,10 +139,16 @@ const Onboard2 = () => {
           pageIndexCallback={changePageHandler}
           containerStyles={{
             paddingHorizontal: wp("5%"),
+            flex: 1,
+          }}
+          imageContainerStyles={{
+            paddingBottom: 0,
+            marginBottom: 0,
           }}
           pages={[
             {
               title: null,
+              subtitle: <CustomBottomContent pageIndex={0} />,
               backgroundColor: pagesBackgroundColor[0],
               image: (
                 <Image
@@ -107,6 +159,7 @@ const Onboard2 = () => {
             },
             {
               title: null,
+              subtitle: <CustomBottomContent pageIndex={1} />,
               backgroundColor: pagesBackgroundColor[1],
               image: (
                 <Image
@@ -117,6 +170,7 @@ const Onboard2 = () => {
             },
             {
               title: null,
+              subtitle: <CustomBottomContent pageIndex={2} />,
               backgroundColor: pagesBackgroundColor[2],
               image: (
                 <Image
@@ -127,6 +181,7 @@ const Onboard2 = () => {
             },
             {
               title: null,
+              subtitle: <CustomBottomContent pageIndex={3} />,
               backgroundColor: pagesBackgroundColor[3],
               image: (
                 <View style={doubleImageContainer as ViewStyle}>
@@ -143,7 +198,6 @@ const Onboard2 = () => {
             },
           ]}
         />
-        <CustomBottomComponent pageIndex={pageIndex} />
       </SafeAreaView>
     </>
   );
@@ -154,5 +208,63 @@ export default Onboard2;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  subtitleWrapper: {
+    width: wp("100%"),
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  bottomContainer: {
+    width: "100%",
+    backgroundColor: "#000",
+    paddingHorizontal: wp("2%"),
+    paddingVertical: hp("1%"),
+    height: hp("45%"),
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  bigText: {
+    color: "#fff",
+    fontSize: wp("7%"),
+    fontWeight: "bold",
+    textAlign: "left",
+    marginTop: hp("2%"),
+    letterSpacing: 0.1,
+    textTransform: "uppercase",
+    fontFamily: "TankerRegular",
+  },
+  paginationContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  paginationDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#666",
+    marginHorizontal: 5,
+  },
+  paginationDotActive: {
+    backgroundColor: "#fff",
+    width: wp("5%"),
+    height: hp("1%"),
+  },
+  button: {
+    backgroundColor: "#FF7A1B",
+    paddingVertical: hp("2%"),
+    borderRadius: wp("10%"),
+    marginBottom: hp("2%"),
+    width: wp("80%"),
+    alignItems: "center",
+  },
+  textContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: hp("2%"),
   },
 });
